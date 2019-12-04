81b85d3e10983ce1e802f6cbb9404c21e7da9c48
Amin Marashi <marashi.s.amin@gmail.com>
2019-12-04 12:14:16 +0800

    Add more strategies

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const api = new DerivAPI({
     connection: new WebSocket('wss://www.binaryqa40.com/websockets/v3?app_id=1009&l=EN&brand=deriv'),
 });
 
-const log = (...args) => console.log(...args);
-
 const analysisStake = 1;
 const realStake = 10;
 
@@ -26,12 +24,12 @@ const bbs = ticks => bollingerBandsArray(ticks.map(t => t.quote.value), bbOption
 const bb = ticks => bollingerBands(ticks.map(t => t.quote.value), bbOptions);
 
 class Strategy {
-    constructor({ api, ticks, profitEvent, bollingerBands, analysisStake }) {
+    constructor({ api, ticks, profitEvent, bollingerBands, stake }) {
         this.api = api;
         this.ticks = ticks;
         this.profitEvent = profitEvent;
         this.bollingerBands = bollingerBands;
-        this.stake = analysisStake;
+        this.stake = stake;
         this.profit = 0;
 
         this.init();
@@ -53,7 +51,7 @@ class Strategy {
             contract_type: type,
             deal_cancellation: 0, duration_unit: 's',
             limit_order:{},
-            multiplier: 100,
+            multiplier: 200,
             product_type: 'basic',
             symbol,
         });
@@ -70,7 +68,7 @@ class Strategy {
                     subscriber.unsubscribe();
                     this.createNewContract(contract.type);
                     this.profit += contract.profit.signed;
-                    log(`New profit for ${this.name}, ${type}: ${this.profit}`);
+                    this.profitEvent.next({ name: this.name, contract });
                 }
             }
         });
@@ -84,10 +82,17 @@ class Strategy {
         return contract.sell();
     }
 
+    log() {
+        console.log(this.name);
+    }
+
     buyCondition(contract) {
     }
 
     sellCondition(contract) {
+        if (contract.profit.signed > this.stake * 1.5) return true;
+        if (contract.profit.signed < -1 * this.stake) return true;
+        return false;
     }
 }
 
@@ -97,81 +102,171 @@ class WhenPassMiddle extends Strategy {
         this.name = 'when pass middle';
     }
 
+    log(contract) {
+        if (contract.type === 'MULTUP') {
+            console.log('spot passed middle, rise');
+        } else {
+            console.log('spot passed middle, fall');
+        }
+    }
+
     buyCondition(contract) {
         const { type } = contract;
         const [ middle ] = bb(this.ticks.list);
         const [ previousSpot, spot ] = this.ticks.list.slice(-2).map(t => t.quote.value);
 
-        if (type === 'MULTUP' && previousSpot < middle && spot > middle) {
-            log('spot passed middle, rise');
-            return true;
-        }
-        if (type === 'MULTDOWN' && previousSpot > middle && spot < middle) {
-            log('spot passed middle, fall');
+        if (
+            (type === 'MULTUP' && previousSpot < middle && spot > middle) ||
+            (type === 'MULTDOWN' && previousSpot > middle && spot < middle)
+        ) {
+            this.log(contract);
             return true;
         }
 
         return false;
     }
+}
 
-    sellCondition(contract) {
-        return contract.profit > 0;
+class WhenTouchEdge extends Strategy {
+    constructor(...args) {
+        super(...args);
+        this.name = 'when touch edge';
+    }
+
+    log(contract) {
+        if (contract.type === 'MULTUP') {
+            console.log('spot touched upper, rise');
+        } else {
+            console.log('spot touched lower, fall');
+        }
+    }
+
+    buyCondition(contract) {
+        const { type } = contract;
+        const [ middle, upper, lower ] = bb(this.ticks.list);
+        const [ spot ] = this.ticks.list.slice(-1).map(t => t.quote.value);
+
+        if (
+            (type === 'MULTUP' && spot === upper) ||
+            (type === 'MULTDOWN' && spot === lower)
+        ) {
+            this.log(contract);
+            return true;
+        }
+
+        return false;
+    }
+}
+
+class WhenTrendAndPassMiddle extends WhenPassMiddle {
+    constructor(...args) {
+        super(...args);
+        this.name = 'when trend and pass middle';
+    }
+
+    log(contract) {
+        if (contract.type === 'MULTUP') {
+            console.log('spot passed middle, and there is a up trend');
+        } else {
+            console.log('spot passed middle, and there is a down trend');
+        }
+    }
+
+    buyCondition(contract) {
+        const [ oldSpot ] = this.ticks.list.slice(-1200).map(t => t.quote.value);
+        const [ spot ] = this.ticks.list.slice(-1).map(t => t.quote.value);
+        const passedMiddle = super.buyCondition(contract);
+
+        if (!passedMiddle) return false;
+
+        const diff = spot - oldSpot;
+
+        if (Math.abs(diff) / spot < 0.001) return false;
+
+        if (
+            (diff > 0 && contract.type === 'MULTUP') ||
+            (diff < 0 && contract.type === 'MULTDOWN')
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+class WhenFastTrend extends Strategy {
+    constructor(...args) {
+        super(...args);
+        this.name = 'when there is a fast trend';
+    }
+
+    log(contract) {
+        if (contract.type === 'MULTUP') {
+            console.log('Fast trend up');
+        } else {
+            console.log('Fast trend down');
+        }
+    }
+
+    buyCondition(contract) {
+        const [ oldSpot ] = this.ticks.list.slice(-10).map(t => t.quote.value);
+        const [ spot ] = this.ticks.list.slice(-1).map(t => t.quote.value);
+        const [ middle, upper, lower ] = bb(this.ticks.list);
+
+        const diff = spot - oldSpot;
+
+        if (Math.abs(diff) / spot < 0.0003) return false;
+
+        if (Math.abs(upper - lower) / middle < 0.0006) return false;
+
+        if (
+            (diff > 0 && contract.type === 'MULTUP') ||
+            (diff < 0 && contract.type === 'MULTDOWN')
+        ) {
+            this.log(contract);
+            return true;
+        }
+
+        return false;
     }
 }
 
 async function main () {
     const ticks = await api.ticks(symbol);
     await api.basic.authorize('a1-Myy4fDZAFgAOr6IZZvAOOF1FDSWz4');
-    /*
-    const bollingerBands = bollingerBandsCreator(ticks.list.map(t => t.quote), );
-*/
+
     const profitEvent = new Subject();
 
-    const strategy = new WhenPassMiddle({
-            api,
-            ticks,
-            profitEvent,
-            analysisStake,
-        });
-
-
-
-    /*
+    const options = {
+        api,
+        ticks,
+        profitEvent,
+        stake: analysisStake,
+    };
     const strategies = [
-        new WhenPassMiddle({
-            api,
-            ticks,
-            profitEvent,
-            bollingerBands,
-            analysisStake,
-        }),
-        new WhenTouchEdge({
-            api,
-            ticks,
-            profitEvent,
-            bollingerBands,
-            analysisStake,
-        }),
-        new WhenDownTrend({
-            api,
-            ticks,
-            profitEvent,
-            bollingerBands,
-            analysisStake,
-        }),
+        new WhenPassMiddle(options),
+        new WhenTouchEdge(options),
+        new WhenTrendAndPassMiddle(options),
+        new WhenFastTrend(options),
     ];
 
     let currentStrategy;
-    profitEvent.subscribe(() => {
-        const sortedStrategies = strategies.sort((s1, s2) => s1.profit - s2.profit);
+    profitEvent.subscribe(({ name, contract }) => {
+        console.log(`New profit for ${name}, ${contract.type}: ${contract.profit.signed}`);
+        // Descending
+        const sortedStrategies = strategies.sort((s1, s2) => s2.profit - s1.profit);
     
-        if (currentStrategy !== sortedStrategies[0]) {
-            currentStrategy.stake = analysisStake;
-            [currentStrategy] = sortedStrategies;
-            currentStrategy.stake = realStake;
+        if (!currentStrategy || currentStrategy.name !== sortedStrategies[0].name) {
+            if (currentStrategy) {
+                console.log(`Old strategy: ${currentStrategy.name}`);
+            }
+            const [bestStrategy] = sortedStrategies;
+            currentStrategy = new bestStrategy.constructor({...options, stake: realStake});
+            console.log(`New strategy: ${currentStrategy.name}`);
         }
     });
-    */
+
+    await new Promise(() => {});
 }
 
 main().then(console.log).catch(console.log);
